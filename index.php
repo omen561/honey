b865066ee2b48b42f65a34cc26ecac213bc2834c
omen561 <omen561@gmail.com>
2014-06-11 09:25:17 +0700

    first load

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/style.css" rel="stylesheet" media="screen">
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <title>Пчелкин мед</title>
+    </head>
+    <body>
+        <header>
+            <div class="wrap-container">
+                <div class="column">
+                    <a id="logo"  href="/">
+                        <img src="files/logo.png">
+                    </a>
+                </div>
+                <div class="column">
+                    <div class="menu">
+                        <ul id="main-menu">
+                            <li><a href="#">КАТАЛОГ</a></li>
+                            <li><a href="#">КАК ЗАКАЗАТЬ</a></li>
+                            <li><a href="#">ПОЛЕЗНАЯ ИНФОРМАЦИЯ</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="header-cart-block">
+                        <div class="title">
+                            <a href="/cart">КОРЗИНА <span>0</span></a>
+                        </div>
+                        <div class="description">
+                            <div id="ajaxCartUpdate" class="load-on-view ajax-cart-processed">
+                                <div id="cart-block-contents-ajax" class="cart-empty">
+                                    Нет товаров в корзине
+                                </div>
+                                <div class="cart-quantity">
+                                    100 позиций
+                                </div>
+                                <div class="cart-summ">
+                                    На сумму: 99 999 руб.
+                                </div>
+                                <div class="cart-links">
+                                    <ul class="links">
+                                        <li class="cart-block-view-cart first">
+                                            <a href="/cart" rel="nofollow">Перейти в корзину</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <a id="header-call-link" class="header-link" href="/">НАПИШИТЕ НАМ</a>
+                </div>
+                <div class="column last">
+                    <a id="header-order-link-box"></a>
+                    <a id="header-order-link" href="/">ЗАКАЗАТЬ</a>
+
+                </div>
+            </div>
+        </header>
+    <main>
+        <div id="front-slider">
+            <div class="wrap-container">
+                <div class="slider">
+                    <ul>
+                        <li>
+                            <img src="files/slide1.png">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div id="promotions">
+            <div class="wrap-container">
+                <div class="list-promotions">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                <div class="promotion">100% натуральный<br/>Алтайский мёд</div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="promotion">100% натуральный<br/>Алтайский мёд</div
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <div class="promotion">100% натуральный<br/>Алтайский мёд</div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="bottom-border"></div>
+        </div>
+        <div class="wrap-container">
+
+        </div>
+    </main>
+    <footer>
+        <div class="wrap-container">
+
+        </div>
+    </footer>
+</body>
+</html>
